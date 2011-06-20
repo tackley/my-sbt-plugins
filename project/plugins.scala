a2dd1154eb78dd106d16596f462c8f6f26fc5766
Graham Tackley <graham.tackley@guardian.co.uk>
2011-06-20 14:55:19 +0100

    Added sh plugin

--- project/plugins.scala
+++ project/plugins.scala
@@ -1,5 +1,6 @@
 import sbt._
 
 object Plugins extends Build {
- lazy val root = Project("root", file(".")) dependsOn (uri("git://github.com/ijuma/sbt-idea.git#sbt-0.10"))
+ lazy val root = Project("root", file("."))
+	.dependsOn (uri("git://github.com/ijuma/sbt-idea.git#sbt-0.10"), uri("git://github.com/steppenwells/sbt-sh.git"))
 }
