16023caee2b30b6fea139fe1f1dbb4622a0a497a
Graham Tackley <graham.tackley@guardian.co.uk>
2011-07-18 10:20:20 +0100

    Use "official" sbt-idea plugin

--- project/plugins.scala
+++ project/plugins.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Plugins extends Build {
  lazy val root = Project("root", file("."))
-	.dependsOn (uri("git://github.com/ijuma/sbt-idea.git#sbt-0.10"), uri("git://github.com/steppenwells/sbt-sh.git"))
+	.dependsOn (uri("git://github.com/mpeltonen/sbt-idea.git#sbt-0.10"), uri("git://github.com/steppenwells/sbt-sh.git"))
 }
