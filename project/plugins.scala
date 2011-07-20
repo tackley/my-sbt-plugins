8e06cf848e88d320ef0e62ba0b474abfa7fd2024
Graham Tackley <graham.tackley@guardian.co.uk>
2011-07-20 16:55:41 +0100

    Use a project name other than root, to make sbt output less confusing

--- project/plugins.scala
+++ project/plugins.scala
@@ -1,6 +1,9 @@
 import sbt._
 
 object Plugins extends Build {
- lazy val root = Project("root", file("."))
-	.dependsOn (uri("git://github.com/mpeltonen/sbt-idea.git#sbt-0.10"), uri("git://github.com/steppenwells/sbt-sh.git"))
+  lazy val root = Project("global-plugins", file("."))
+    .dependsOn(
+      uri("git://github.com/mpeltonen/sbt-idea.git#sbt-0.10"), 
+      uri("git://github.com/steppenwells/sbt-sh.git")
+    )
 }
