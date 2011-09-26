62eb9c95dc4e6ecd56aa1e881ce8682ee97f051d
Robbie Clutton <rclutton@gnm41091.int.gnl>
2011-09-26 11:00:04 +0100

    alter for changes to sbt-idea branch changes

--- project/plugins.scala
+++ project/plugins.scala
@@ -3,7 +3,7 @@ import sbt._
 object Plugins extends Build {
   lazy val root = Project("global-plugins", file("."))
     .dependsOn(
-      uri("git://github.com/mpeltonen/sbt-idea.git"), 
       uri("git://github.com/steppenwells/sbt-sh.git")
     )
 }
+
