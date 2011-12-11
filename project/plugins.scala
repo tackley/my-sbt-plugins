86c50fd015ca4647335c380cb067d73f9eb0d199
Graham Tackley <graham@tackley.net>
2011-12-11 21:32:39 +0000

    Removed sbt-sh
    (Sorry swells,  but source plugin dependencies here really slow down
    switching sbt versions.)

--- project/plugins.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object Plugins extends Build {
-  lazy val root = Project("global-plugins", file("."))
-    .dependsOn(
-      uri("git://github.com/steppenwells/sbt-sh.git")
-    )
-}
